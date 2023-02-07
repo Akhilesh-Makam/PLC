9468a29ddc3e236a5adceb4554817f9ddb126ebc
makam <makam@MSI>
2023-02-06 19:00:49 -0500

    first implementation of next()

--- edu/ufl/cise/plcsp23/Scanner.java
+++ edu/ufl/cise/plcsp23/Scanner.java
@@ -1,33 +1,28 @@
 package edu.ufl.cise.plcsp23.edu.ufl.cise.plcsp23;
 
 import java.util.HashMap;
-import java.util.*;
-
-
 public class Scanner implements IScanner,IToken {
-
     private SourceLocation sourceLocation;
     private Kind kind;
     private String tokenString;
     private String input;
-
     private int currentIndex;
-    private String now;
-    private char current;
     private int currentLine;
     private int currentColumn;
-    public static HashMap <String, Kind> reservedWords;
-    
-    
+    private int limit;
+    public static HashMap<String, Kind> reservedWords;
+    public static HashMap<String, Kind> operators;
+
     //defining states of DFA
-    private enum State{
-    	START, 
-    	HAVE_EQ, 
-    	IN_IDENT,
-    	IN_NUM_LIT
+    private enum State {
+        START,
+        OP,
+        IN_IDENT,
+        IN_NUM_LIT,
+        IN_STRING_LIT
     }
 
-    static{
+    static {
         reservedWords.put("image", Kind.RES_image);
         reservedWords.put("pixel", Kind.RES_pixel);
         reservedWords.put("int", Kind.RES_int);
@@ -54,30 +49,274 @@ public class Scanner implements IScanner,IToken {
         reservedWords.put("atan", Kind.RES_atan);
         reservedWords.put("if", Kind.RES_if);
         reservedWords.put("while", Kind.RES_while);
+
+        operators.put(".", Kind.DOT);
+        operators.put(",", Kind.COMMA);
+        operators.put("?", Kind.QUESTION);
+        operators.put(":", Kind.COLON);
+        operators.put("(", Kind.LPAREN);
+        operators.put(")", Kind.RPAREN);
+        operators.put("<", Kind.LT);
+        operators.put(">", Kind.GT);
+        operators.put("[", Kind.LSQUARE);
+        operators.put("]", Kind.RSQUARE);
+        operators.put("{", Kind.LCURLY);
+        operators.put("}", Kind.RCURLY);
+        operators.put("=", Kind.ASSIGN);
+        operators.put("==", Kind.EQ);
+        operators.put("<->", Kind.EXCHANGE);
+        operators.put("<=", Kind.LE);
+        operators.put(">=", Kind.GE);
+        operators.put("!", Kind.BANG);
+        operators.put("&", Kind.BITAND);
+        operators.put("&&", Kind.AND);
+        operators.put("|", Kind.BITOR);
+        operators.put("||", Kind.OR);
+        operators.put("+", Kind.PLUS);
+        operators.put("-", Kind.MINUS);
+        operators.put("*", Kind.TIMES);
+        operators.put("**", Kind.EXP);
+        operators.put("/", Kind.DIV);
+        operators.put("%", Kind.MOD);
     }
 
-
     public Scanner(String input) {
-
-        now = "";
         this.input = input;
         currentIndex = 0;
         currentLine = 1;
         currentColumn = 0;
-        
+        limit = input.length();
+    }
 
+    public IToken next() throws LexicalException {
+        int startLine = currentLine;
+        int startColumn = currentColumn;
+
+        State state = State.START;
+        tokenString = "";
+        char c = input.charAt(currentIndex);
+
+        if (currentIndex < limit) {
+            boolean comment = false;
+            if (c == '~') {
+                comment = true;
+                while ((c != 'r' || c != 'n') && currentIndex < limit) {
+                    currentIndex++;
+                    c = input.charAt(currentIndex);
+                }
+            }
+            if (comment) {
+                currentLine++;
+                currentColumn = 0;
+            }
+            if (!(currentIndex < limit)) {
+                kind = Kind.EOF;
+                sourceLocation = new SourceLocation(startLine, startColumn);
+                return this;
+            } else if (comment) {
+                startLine = currentLine;
+                startColumn = currentColumn;
+            }
+
+            boolean white = false;
+            while(Character.isWhitespace(c) && currentIndex < limit){
+                white = true;
+                if(c == ' '){
+                    currentIndex++;
+                    currentColumn++;
+                }
+                else if(c == '\n'){
+                    currentLine++;
+                    currentColumn = 0;
+                }
+                else if(c == '\t'){
+                    currentColumn += 4;
+                }
+                c = input.charAt(currentIndex);
+            }
+            if (!(currentIndex < limit)) {
+                kind = Kind.EOF;
+                sourceLocation = new SourceLocation(startLine, startColumn);
+                return this;
+            } else if (white) {
+                startLine = currentLine;
+                startColumn = currentColumn;
+            }
+
+
+            if (Character.isDigit(c)) {
+                state = State.IN_NUM_LIT;
+            } else if (Character.isLetter(c) || c == '_') {
+                state = State.IN_IDENT;
+            } else if (operators.containsKey(c)) {
+                state = State.OP;
+                kind = operators.get(c);
+            } else if (c == '"'){
+                state = State.IN_STRING_LIT;
+            }
+            else {
+                kind = Kind.ERROR;
+                sourceLocation = new SourceLocation(startLine, startColumn);
+                currentColumn++;
+                currentIndex++;
+                return this;
+            }
         }
 
-    public void found(SourceLocation sourceLocation, Kind kind, String tokenString){
-        this.sourceLocation = sourceLocation;
-        this.kind = kind;
-        this.tokenString = tokenString;
+        if (state == State.IN_IDENT) {
+            tokenString += c;
+            currentIndex++;
+            while (currentIndex < limit) {
+                c = input.charAt(currentIndex);
+                if (Character.isLetterOrDigit(c) || c == '_') {
+                    tokenString += c;
+                    currentIndex++;
+                    currentColumn++;
+                } else if (Character.isWhitespace(c)) {
+                    Kind k = reservedWords.get(tokenString);
+                    if (k == null) {
+                        kind = Kind.IDENT;
+                    } else {
+                        kind = k;
+                    }
+                    sourceLocation = new SourceLocation(startLine, startColumn);
+                    return this;
+                } else {
+                    throw new LexicalException("Invalid character/number in Identifier");
+                }
+            }
+        } else if (state == State.IN_NUM_LIT) {
+            tokenString += c;
+            currentIndex++;
+            while(currentIndex < limit){
+                c = input.charAt(currentIndex);
+                if(Character.isDigit(c)){
+                    tokenString += c;
+                    currentIndex++;
+                    currentColumn++;
+                }
+                else if(Character.isWhitespace(c)){
+                    kind = Kind.NUM_LIT;
+                    sourceLocation = new SourceLocation(startLine, startColumn);
+                    long x = Integer.parseInt(tokenString);
+                    if(x > 2147483647 || x < -2147483648){
+                        throw new LexicalException("Number out of range for NUM_LIT");
+                    }else{
+                        return this;
+                    }
+                }
+                else{
+                    throw new LexicalException("Invalid character in NUM_LIT");
+                }
+            }
+
+        } else if (state == State.OP) {
+            if (kind == Kind.ASSIGN) {
+                int x = currentIndex + 1;
+                if (x < limit && input.charAt(x) == '=') {
+                    tokenString += '=';
+                    kind = Kind.EQ;
+                    currentIndex += 2;
+                    currentColumn += 2;
+                } else {
+                    currentColumn++;
+                    currentIndex++;
+                }
+            } else if (kind == Kind.LT) {
+                int x = currentIndex + 1;
+                if (x < limit && input.charAt(x) == '=') {
+                    tokenString += '=';
+                    kind = Kind.LE;
+                    currentIndex += 2;
+                    currentColumn += 2;
+                } else if (x < limit && input.charAt(x) == '-') {
+                    tokenString += '-';
+                    int y = currentIndex + 2;
+                    currentIndex++;
+                    if (y < limit && input.charAt(y) == '>') {
+                        tokenString += '>';
+                        kind = Kind.EXCHANGE;
+                        currentIndex += 2;
+                        currentColumn += 2;
+                    } else {
+                        kind = Kind.ERROR;
+                        currentIndex++;
+                        currentColumn++;
+                    }
+                }
+            } else if (kind == Kind.GT) {
+                int x = currentIndex + 1;
+                if (x < limit && input.charAt(x) == '=') {
+                    tokenString += '=';
+                    kind = Kind.GE;
+                    currentIndex += 2;
+                    currentColumn += 2;
+                } else {
+                    currentIndex++;
+                    currentColumn++;
+                }
+            } else if (kind == Kind.BITAND) {
+                int x = currentIndex + 1;
+                if (x < limit && input.charAt(x) == '&') {
+                    tokenString += '&';
+                    kind = Kind.AND;
+                    currentIndex += 2;
+                    currentColumn += 2;
+                } else {
+                    currentColumn++;
+                    currentIndex++;
+                }
+            } else if (kind == Kind.BITOR) {
+                int x = currentIndex + 1;
+                if (x < limit && input.charAt(x) == '|') {
+                    tokenString += '|';
+                    kind = Kind.OR;
+                    currentIndex += 2;
+                    currentColumn += 2;
+                } else {
+                    currentColumn++;
+                    currentIndex++;
+                }
+            } else if (kind == Kind.TIMES) {
+                int x = currentIndex + 1;
+                if (x < limit && input.charAt(x) == '*') {
+                    tokenString += '*';
+                    kind = Kind.EXP;
+                    currentIndex += 2;
+                    currentColumn += 2;
+                } else {
+                    currentColumn++;
+                    currentIndex++;
+                }
+            } else {
+                currentColumn++;
+                currentIndex++;
+            }
+            sourceLocation = new SourceLocation(startLine, startColumn);
+            return this;
+        }else if(state == State.IN_STRING_LIT){
+            tokenString += c;
+            currentColumn++;
+            currentIndex++;
+            boolean close = false;
+            while(currentIndex < limit || !close){
+                c = input.charAt(currentIndex);
+                if (c=='\"'){
+                    close = true;
+                }
+                tokenString += c;
+                currentIndex++;
+                currentColumn++;
+            }
+            kind = Kind.STRING_LIT;
+            sourceLocation = new SourceLocation(startLine, startColumn);
+            return this;
+
+        }
+        throw new LexicalException("No token found");
+
     }
 
-    public boolean isWhiteSpace(char c){
-        return (c == 32 || c == 13 || c == 10 || c == 9 || c == 12);
-    }
-    public IToken next() throws LexicalException {return null;}
 
 
     @Override
@@ -88,6 +327,7 @@ public class Scanner implements IScanner,IToken {
     public Kind getKind(){return kind;}
     @Override
     public String getTokenString(){return tokenString;}
+
 
 
 }
