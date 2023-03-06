589fbe957f5499c6a67e63ee61dcd07eac4655ac
makam <makam@MSI>
2023-03-05 23:19:29 -0500

    reread class notes and textbook, fixed parse to be top down parser, pass 2/3 of tests, seems like the other tests might be failing due to lexer

--- Parser.java
+++ Parser.java
@@ -1,6 +1,7 @@
 package edu.ufl.cise.plcsp23;
 
-import java.util.Stack;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Vector;
 
 import edu.ufl.cise.plcsp23.IToken.Kind;
@@ -9,33 +10,30 @@ import edu.ufl.cise.plcsp23.ast.*;
 import edu.ufl.cise.plcsp23.ast.AST;
 import edu.ufl.cise.plcsp23.ast.BinaryExpr;
 import edu.ufl.cise.plcsp23.ast.ConditionalExpr;
-import edu.ufl.cise.plcsp23.ast.IdentExpr;
-import edu.ufl.cise.plcsp23.ast.NumLitExpr;
-import edu.ufl.cise.plcsp23.ast.RandomExpr;
-import edu.ufl.cise.plcsp23.ast.StringLitExpr;
-import edu.ufl.cise.plcsp23.ast.UnaryExpr;
-import edu.ufl.cise.plcsp23.ast.ZExpr;
+
 import static edu.ufl.cise.plcsp23.IToken.Kind.*;
 
 public class Parser implements IParser {
 
     private Scanner scanner;
-    private AST first;
+    private int current;
     private String input;
     private Vector<IToken> tokens;
-
+    private AST first;
 
 
     //have to make Parser class and ASTVisitor Class
     public Parser(String input, Scanner scanner) {
         this.scanner = scanner;
         this.input = input;
+        current = 0;
+
     }
 
     @Override
     public AST parse() throws PLCException {//where the actual parsing occurs
-        Vector<IToken> tokens = new Vector<>();
         IToken current = scanner.next();
+        tokens = new Vector<>();
         tokens.add(current);
         if (current.getKind() == Kind.EOF) {
             throw new SyntaxException("No input");
@@ -54,53 +52,166 @@ public class Parser implements IParser {
 //            throw new SyntaxException("Invalid sequence of tokens");
 //        }
 
+        //unable to get the AST generation working
 
-        int ifCount = 0;
-        int questionCount = 0;
-        Stack<AST> stack = new Stack<>();
-        for (int i = 0; i < tokens.size(); i++) {
-            IToken token = tokens.get(i);
-            switch (token.getKind()) {
-                case IDENT:
-                    stack.push(new IdentExpr(token));
-                    break;
-                case NUM_LIT:
-                    stack.push(new NumLitExpr(token));
-                    break;
-                case RES_rand:
-                    stack.push(new RandomExpr(token));
-                    break;
-                case STRING_LIT:
-                    stack.push(new StringLitExpr(token));
-                    break;
-                case RES_Z:
-                    stack.push(new ZExpr(token));
-                case BANG:
-                case RES_sin:
-                case RES_cos:
-                case RES_atan:
-                    //unary stuff goes here, haven't added minus since it can be in additive
-                case EOF:
-                    continue;
-                default:
-                    throw new SyntaxException("Invalid sequence of tokens");
+        return expr();
+    }
+
+    private boolean match(Kind...expected){
+        if (current >= tokens.size()){
+            return false;
+        }
+        for(Kind actual : expected){
+            if(check(actual)){
+                current++;
+                return true;
             }
         }
-
-
-        while(stack.size() != 1){
-            stack.pop();
-        }
-
-        first = stack.peek();
-        return first;
-
-
-
-
-
-        //unable to get the AST generation working
+        return false;
     }
+
+    private boolean check(Kind expected){
+        if (current >= tokens.size()){
+            return false;
+        }
+        return tokens.get(current).getKind() == expected;
+    }
+
+    private IToken previous(){
+        return tokens.get(current - 1);
+    }
+
+    private void expect(Kind expected) throws PLCException{
+        if(check(expected)==false){
+            throw new PLCException("Unexpected token");
+        }
+        current++;
+    }
+    private Expr expr() throws PLCException{
+        if (match(RES_if)){
+            return conditionalExpr();
+        }
+        return orExpr();
+    }
+
+    private Expr conditionalExpr() throws PLCException{
+        IToken tmp = tokens.get(current);
+        Expr condition = expr();
+        expect(QUESTION);
+        Expr then = expr();
+        expect(QUESTION);
+        Expr elseExpr = expr();
+        return new ConditionalExpr(tmp, condition, then, elseExpr);
+    }
+
+    private Expr orExpr() throws PLCException{
+        Expr first = andExpr();
+        IToken tmp = tokens.get(current);
+        while(match(OR) || match(BITOR)){
+            Kind op = previous().getKind();
+            Expr second = andExpr();
+            first = new BinaryExpr(tmp, first, op, second);
+            tmp = tokens.get(current);
+        }
+        return first;
+    }
+
+    private Expr andExpr() throws PLCException{
+        Expr first = comparisonExpr();
+        IToken tmp = tokens.get(current);
+        while(match(AND) || match(BITAND)){
+            Kind op = previous().getKind();
+            Expr second = comparisonExpr();
+            first = new BinaryExpr(tmp, first, op, second);
+            tmp = tokens.get(current);
+        }
+        return first;
+    }
+
+    private Expr comparisonExpr() throws PLCException{
+        Expr first = powerExpr();
+        IToken tmp = tokens.get(current);
+        while(match(LT,LE,GT,GE)){
+            Kind op = previous().getKind();
+            Expr second = powerExpr();
+            first = new BinaryExpr(tmp, first, op, second);
+            tmp = tokens.get(current);
+        }
+        return first;
+    }
+
+    private Expr powerExpr() throws PLCException {
+        Expr first = additiveExpr();
+        IToken tmp = tokens.get(current);
+        if (match(EXP)) {
+            Expr right = powerExpr();
+            return new BinaryExpr(tmp, first, EXP, right);
+        }
+        return first;
+    }
+
+    private Expr additiveExpr() throws PLCException {
+        Expr first = multiplicativeExpr();
+        IToken tmp = tokens.get(current);
+        while (match(PLUS, MINUS)) {
+            Kind op = previous().getKind();
+            Expr second = multiplicativeExpr();
+            first = new BinaryExpr(tmp, first, op, second);
+        }
+        return first;
+    }
+
+    private Expr multiplicativeExpr() throws PLCException {
+        Expr first = unaryExpr();
+        IToken tmp = tokens.get(current);
+        while (match(TIMES, DIV, MOD)) {
+            Kind op = previous().getKind();
+            Expr second = unaryExpr();
+            first = new BinaryExpr(tmp, first, op, second);
+        }
+        return first;
+    }
+
+    private Expr unaryExpr() throws PLCException {
+        if (match(BANG, MINUS, RES_sin, RES_cos, RES_atan)) {
+            IToken tmp = tokens.get(current);
+            Kind op = previous().getKind();
+            Expr expr = unaryExpr();
+            return new UnaryExpr(tmp, op, expr);
+        }
+        return primaryExpr();
+    }
+    private Expr primaryExpr() throws PLCException {
+        if (match(STRING_LIT)) {
+            return new StringLitExpr(previous());
+        } else if (match(NUM_LIT)) {
+            return new NumLitExpr(previous());
+        } else if (match(IDENT)) {
+            return new IdentExpr(previous());
+        } else if (match(LPAREN)) {
+            Expr f = expr();
+            while(current < tokens.size()){
+                if(!match(RPAREN)){
+                    f = expr();
+                }
+                else{
+                    return f;
+                }
+            }
+            if(current >= tokens.size()){
+                throw new PLCException("Expected primary expression");
+            }
+
+        }
+        else if(match(RES_Z)){
+            return new ZExpr(previous());
+        }
+        else if(match(RES_rand)){
+            return new RandomExpr(previous());
+        }
+        throw new PLCException("Token not yet implemented or DNE");
+    }
+
 
 //    private boolean legal(Vector<IToken> tokens) { //did not understand the CONSUME stuff so just did switch statements
 //        int ifCount = 0;
