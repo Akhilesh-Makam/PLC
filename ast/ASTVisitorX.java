9c5a5930226229a6aaecf3f9fd2c50ae88eb6dd5
makam <makam@MSI>
2023-03-27 13:04:29 -0400

    made visitBinaryExpr, symbolTable class, check, visitpreDeclaredVarExpr, visitStringLitExpr, visitNumLitExpr, visitRandomExpr, visitStringLitExpr, and visitZexpr

--- ast/ASTVisitorX.java
+++ ast/ASTVisitorX.java
@@ -1,10 +1,34 @@
 package edu.ufl.cise.plcsp23.ast;
 
+import edu.ufl.cise.plcsp23.IToken;
+import edu.ufl.cise.plcsp23.Token;
 import edu.ufl.cise.plcsp23.PLCException;
+import edu.ufl.cise.plcsp23.TypeCheckException;
+
+import java.util.HashMap;
 
 
 public class ASTVisitorX implements ASTVisitor{
 
+    public static class SymbolTable{
+        HashMap<String, Declaration> entries = new HashMap<>();
+
+        public boolean insert(String name, Declaration declaration){
+            return (entries.putIfAbsent(name, declaration) == null);
+        }
+
+        public Declaration lookup(String name){
+            return entries.get(name);
+        }
+    }
+
+    SymbolTable symbolTable = new SymbolTable();
+
+    private void check(boolean condtion, AST node, String message) throws PLCException{
+        if(!condtion){
+            throw new TypeCheckException(message + node.getColumn() + node.getLine());
+        }
+    }
     @Override
     public Object visitAssignmentStatement(AssignmentStatement statementAssign, Object arg) throws PLCException {
         return null;
@@ -12,7 +36,71 @@ public class ASTVisitorX implements ASTVisitor{
 
     @Override
     public Object visitBinaryExpr(BinaryExpr binaryExpr, Object arg) throws PLCException {
-        return null;
+       Token.Kind op = binaryExpr.getOp();
+       Type left = (Type) binaryExpr.getLeft().visit(this, arg);
+       Type right = (Type) binaryExpr.getRight().visit(this, arg);
+       Type result = null;
+       switch(op){
+           case BITAND, BITOR ->{
+               check((left == Type.PIXEL) && (right == Type.PIXEL),binaryExpr, "incompatible types with given operator");
+               result = Type.PIXEL;
+           }
+           case AND, OR, LT, GT, LE, GE ->{
+               check((left == Type.INT) && (right == Type.INT),binaryExpr, "incompatible types with given operator");
+               result = Type.INT;
+           }
+           case EQ ->{
+               check((left == Type.INT && right == Type.INT) || (left == Type.PIXEL && right == Type.PIXEL)
+                       || (left == Type.IMAGE && right == Type.IMAGE) || (left == Type.STRING && right == Type.STRING),
+                       binaryExpr, "incompatible types with given operator");
+               result = Type.INT;
+           }
+           case EXP->{
+               check((left == Type.INT && right == Type.INT) || (left == Type.PIXEL && right == Type.INT)
+                               || (left == Type.INT && right == Type.PIXEL), binaryExpr,
+                       "incompatible types with given operator");
+               if(left == Type.PIXEL || right == Type.PIXEL){
+                   result = Type.PIXEL;
+               }
+               else{
+                   result = Type.INT;
+               }
+           }
+           case PLUS -> {
+               check((left == Type.INT && right == Type.INT) || (left == Type.PIXEL && right == Type.PIXEL)
+                               || (left == Type.IMAGE && right == Type.IMAGE) || (left == Type.STRING && right == Type.STRING),
+                       binaryExpr, "incompatible types with given operator");
+               result = left;
+           }
+           case MINUS -> {
+               check((left == Type.INT && right == Type.INT) || (left == Type.PIXEL && right == Type.PIXEL)
+                               || (left == Type.IMAGE && right == Type.IMAGE),
+                       binaryExpr, "incompatible types with given operator");
+               result = left;
+           }
+           case TIMES, DIV, MOD -> {
+               check((left == Type.INT && right == Type.INT) || (left == Type.PIXEL && right == Type.PIXEL)
+                               || (left == Type.IMAGE && right == Type.IMAGE) || (left == Type.PIXEL && right == Type.INT)
+                               || (left == Type.INT && right == Type.PIXEL) || (left == Type.IMAGE && right == Type.INT) ||
+                               (left == Type.INT && right == Type.IMAGE)
+                       ,
+                       binaryExpr, "incompatible types with given operator");
+               if(left == Type.PIXEL || right == Type.PIXEL){
+                   result = Type.PIXEL;
+               }
+               else if(left == Type.IMAGE || right == Type.IMAGE){
+                   result = Type.IMAGE;
+               }
+               else{
+                   result = left;
+               }
+           }
+           default -> {
+               throw new TypeCheckException("Incompatible operator in BinaryExpr");
+           }
+       }
+       binaryExpr.setType(result);
+       return result;
     }
 
     @Override
@@ -62,7 +150,8 @@ public class ASTVisitorX implements ASTVisitor{
 
     @Override
     public Object visitNumLitExpr(NumLitExpr numLitExpr, Object arg) throws PLCException {
-        return null;
+        numLitExpr.setType(Type.INT);
+        return Type.INT;
     }
 
     @Override
@@ -77,7 +166,8 @@ public class ASTVisitorX implements ASTVisitor{
 
     @Override
     public Object visitPredeclaredVarExpr(PredeclaredVarExpr predeclaredVarExpr, Object arg) throws PLCException {
-        return null;
+        predeclaredVarExpr.setType(Type.INT);
+        return Type.INT;
     }
 
     @Override
@@ -87,7 +177,8 @@ public class ASTVisitorX implements ASTVisitor{
 
     @Override
     public Object visitRandomExpr(RandomExpr randomExpr, Object arg) throws PLCException {
-        return null;
+        randomExpr.setType((Type.INT));
+        return Type.INT;
     }
 
     @Override
@@ -97,7 +188,8 @@ public class ASTVisitorX implements ASTVisitor{
 
     @Override
     public Object visitStringLitExpr(StringLitExpr stringLitExpr, Object arg) throws PLCException {
-        return null;
+       stringLitExpr.setType(Type.STRING);
+       return Type.STRING;
     }
 
     @Override
@@ -122,6 +214,7 @@ public class ASTVisitorX implements ASTVisitor{
 
     @Override
     public Object visitZExpr(ZExpr zExpr, Object arg) throws PLCException {
-        return zExpr.getValue();
+        zExpr.setType(Type.INT);
+        return Type.INT;
     }
 }
